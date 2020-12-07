a70f2204a456965b52838a2851680bde8b1cbeb5
Saketh Mynampati <smynampati42@gmail.com>
2020-12-07 18:38:31 -0500

    cleaned Drivetrain.java

--- src/main/java/frc/robot/subsystems/Drivetrain.java
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -1,21 +1,14 @@
 package frc.robot.subsystems;
 
-import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
-
-import edu.wpi.first.wpilibj.controller.PIDController;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot.Robot;
-import frc.robot.operator.controllers.BionicF310;
 
 public class Drivetrain extends SubsystemBase {
 
     private CANSparkMax left, right;
     private DifferentialDrive diffDrive;
-    private PIDController pidController;
 
     public Drivetrain(int l, int r) {
         
